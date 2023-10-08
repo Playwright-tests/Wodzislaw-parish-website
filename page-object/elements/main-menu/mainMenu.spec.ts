4d444351aa75827216f58b14e81a9e9f7cbe60b2
aksi1984 <pablo_xmen@op.pl>
2023-10-08 11:31:23 +0200

    Added DropdownList class instance

--- page-object/elements/main-menu/mainMenu.spec.ts
+++ page-object/elements/main-menu/mainMenu.spec.ts
@@ -1,15 +1,25 @@
 import { Page } from "@playwright/test";
 import { BasePage } from "../../base/basePage.spec";
+import { DropdownList } from "./dropdownList.spec";
 
 export class MainMenu extends BasePage {
+
+    private dropdownList: DropdownList;
 
     constructor(page: Page) {
 
         super(page);
+
+        this.dropdownList = new DropdownList(page);
     }
 
     async clickLink(linkText: string) {
 
         await this.page.getByRole('link', {name: linkText}).click();
+    }
+
+    async getDropdownList() {
+
+        return this.dropdownList;
     }
 }
