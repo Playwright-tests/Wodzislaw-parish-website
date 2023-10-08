88ab500ed52ab45a1cc59de67ab3df65562f6570
aksi1984 <pablo_xmen@op.pl>
2023-10-08 22:57:15 +0200

    Created the class

---
+++ page-object/elements/parishGroupsMenu.spec.ts
@@ -0,0 +1,15 @@
+import { Page } from "@playwright/test";
+import { BasePage } from "../base/basePage.spec";
+
+export class ParishGroupsMenu extends BasePage {
+
+    constructor(page: Page) {
+
+        super(page);
+    }
+
+    async clickLink(linkText: string) {
+
+        await this.page.locator('#site-aside').getByRole('link', {name: linkText}).click();
+    }
+}
