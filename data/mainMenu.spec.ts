cb03ee4e02f837eef0aa842750916f4763f7a099
aksi1984 <pablo_xmen@op.pl>
2023-10-08 10:28:09 +0200

    Created the mainMenuLinks array

---
+++ data/mainMenu.spec.ts
@@ -0,0 +1,50 @@
+export const mainMenuLinks = [
+
+    {
+        link: 'Strona główna',
+        pageUrl: 'https://wnmp.pl/',
+        tabName: 'PARAFIA WNIEBOWZIĘCIA NAJŚWIĘTSZEJ MARYI PANNY W WODZISŁAWIU ŚLĄSKIM'
+    },
+
+    {
+        link: 'Ogłoszenia',
+        pageUrl: 'https://wnmp.pl/ogloszenia/',
+        tabName: 'Ogłoszenia'
+    },
+
+    {
+        link: 'Intencje',
+        pageUrl: 'https://wnmp.pl/intencje/',
+        tabName: 'Intencje'
+    },
+
+    {
+        link: 'Duszpasterze',
+        pageUrl: 'https://wnmp.pl/duszpasterze/obecni-duszpasterze/',
+        tabName: 'Obecni duszpasterze'
+    },
+
+    {
+        link: 'Grupy parafialne',
+        pageUrl: 'https://wnmp.pl/grupy-parafialne/',
+        tabName: 'Grupy parafialne'
+    },
+
+    {
+        link: 'Historia parafii',
+        pageUrl: 'https://wnmp.pl/historia-parafii/z-kart-historii/',
+        tabName: 'Z kart historii'
+    },
+
+    {
+        link: 'Galeria zdjęć',
+        pageUrl: 'https://wnmp.pl/media/',
+        tabName: 'Galeria zdjęć'
+    },
+
+    {
+        link: 'Kontakt',
+        pageUrl: 'https://wnmp.pl/kontakt/',
+        tabName: 'Kontakt'
+    }
+]
