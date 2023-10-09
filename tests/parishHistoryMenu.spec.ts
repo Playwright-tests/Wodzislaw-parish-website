2c3a495b10c4c021779f4515a63dd218fbc66945
aksi1984 <pablo_xmen@op.pl>
2023-10-09 23:59:02 +0200

    Added the new fixture to tests

--- tests/parishHistoryMenu.spec.ts
+++ tests/parishHistoryMenu.spec.ts
@@ -1,15 +1,17 @@
 import { parishHistoryMenuLinks } from "../data/parishHistoryMenu.spec";
-import { test, expect } from "../fixtures/parishHistoryMenu.spec";
+import { test, expect } from "../fixtures/subpageMenu_1.spec";
+
+test.use({ mainMenuLink: 'Historia parafii' });
 
 test.describe('Parish history menu tests',async () => {
     
     for(let i = 0; i < parishHistoryMenuLinks.length; i++) {
 
-        test('Clicking the "' + parishHistoryMenuLinks[i].link + '" link',async ({page, parishHistoryMenu}) => {
+        test('Clicking the "' + parishHistoryMenuLinks[i].link + '" link',async ({page, menu}) => {
             
             await test.step('Click the "' + parishHistoryMenuLinks[i].link + '" link',async () => {
                 
-                await parishHistoryMenu.clickLink(parishHistoryMenuLinks[i].link);
+                await menu.clickLink(parishHistoryMenuLinks[i].link);
             })
 
             await expect(page).toHaveURL(parishHistoryMenuLinks[i].pageUrl);
