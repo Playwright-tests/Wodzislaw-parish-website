6f0bd3c0f524a14aa167f5348f4720536cd7a1ff
aksi1984 <pablo_xmen@op.pl>
2023-10-09 23:58:14 +0200

    Added the test step

--- tests/parishGroupsMenu.spec.ts
+++ tests/parishGroupsMenu.spec.ts
@@ -1,15 +1,17 @@
 import { parishGroupsLinks } from "../data/parishGroupsMenu.spec";
-import { test, expect } from "../fixtures/parishGroupsMenu.spec";
+import { test, expect } from "../fixtures/subpageMenu_1.spec";
+
+test.use({ mainMenuLink: 'Grupy parafialne' });
 
 test.describe('Parish groups menu tests',async () => {
     
     for(let i = 0; i < parishGroupsLinks.length; i++) {
 
-        test('Clicking the "' + parishGroupsLinks[i].link + '" link',async ({page, parishGroupsMenu}) => {
+        test('Clicking the "' + parishGroupsLinks[i].link + '" link',async ({page, menu}) => {
             
             await test.step('Click the "' + parishGroupsLinks[i].link + '" link',async () => {
                 
-                await parishGroupsMenu.clickLink(parishGroupsLinks[i].link);
+                await menu.clickLink(parishGroupsLinks[i].link);
             })
 
             await expect(page).toHaveURL(parishGroupsLinks[i].pageUrl);
