627dbbc545c83e0b2f6537f6494971ba9a5aca9f
aksi1984 <pablo_xmen@op.pl>
2023-10-16 09:11:52 +0200

    Created test data for search engine testing

---
+++ data/searchEngine.spec.ts
@@ -0,0 +1,16 @@
+export const correctPhrases = [
+
+    'Organy',
+    'Chór',
+    'Duszpastzerze',
+    'Spowiedź'
+]
+
+export const incorrectPhrases = [
+
+    'dW5kZWZpbmVk',
+    'OTk5OTk5OTk5OTk5OTk5OTk5OTk5OTk5OTk5OTk5OTk5OTk5OTk5OTk5OTk5OTk5OTk5OTk5OTk5',
+    'MHhmZmZmZmZmZmZmZmZmZmZm',
+    '8JuyoPCbsqHwm7Ki8Juyo/CdhbPwnYW08J2FtfCdhbbwnYW38J2FuPCdhbnwnYW686CAgfOggKDz',
+    'QUJDPGRpdiBzdHlsZT0ieDpleHByZXNzaW9ueDAwKGphdmFzY3JpcHQ6YWxlcnQoMSkiPkRFRg=='
+]
