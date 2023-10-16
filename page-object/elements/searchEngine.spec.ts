624833b9801add0f8f7a08e81ff56bd8c1d56929
aksi1984 <pablo_xmen@op.pl>
2023-10-16 07:03:04 +0200

    Created the SearchEngine class

---
+++ page-object/elements/searchEngine.spec.ts
@@ -0,0 +1,26 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "../base/basePage.spec";
+
+export class SearchEngine extends BasePage {
+
+    readonly searchField: Locator;
+    readonly searchButton: Locator;
+
+    constructor(page: Page) {
+
+        super(page);
+
+        this.searchField = page.getByPlaceholder('Szukaj …');
+        this.searchButton = page.getByRole('button', {name: 'Szukaj'});
+    }
+
+    async setPhrase(phrase: string) {
+
+        await this.searchField.fill(phrase);
+    }
+
+    async clickButton() {
+
+        await this.searchButton.click();
+    }
+}
