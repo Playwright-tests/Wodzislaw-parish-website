26f2b1b94f86fe818514b61e6c3374afa2c0cac5
aksi1984 <pablo_xmen@op.pl>
2023-10-16 09:12:31 +0200

    Added the SearchResults class instance

--- fixtures/searchEngine.spec.ts
+++ fixtures/searchEngine.spec.ts
@@ -1,9 +1,11 @@
 import { test as base } from "./subpageMenu_2.spec";
 import { SearchEngine } from "../page-object/elements/searchEngine.spec";
+import { SearchResultsList } from "../page-object/elements/searchResultsList.spec";
 
 type SearchEngineFixture = {
 
-    searchEngine: SearchEngine
+    searchEngine: SearchEngine,
+    searchResultsList: SearchResultsList
 };
 
 export const test = base.extend<SearchEngineFixture>({
@@ -15,6 +17,15 @@ export const test = base.extend<SearchEngineFixture>({
         const searchEngine = new SearchEngine(page);
 
         await use(searchEngine);
+    },
+
+    searchResultsList:async ({page}, use) => {
+        
+        await page.goto('https://wnmp.pl/');
+
+        const searchResultsList = new SearchResultsList(page);
+
+        await use(searchResultsList);
     }
 })
 
