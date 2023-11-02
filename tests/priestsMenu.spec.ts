3c0956ed1e25e5d7f6895ab74d84fea07efa9919
aksi1984 <pablo_xmen@op.pl>
2023-11-02 17:12:15 +0100

    Test data from a json file was used

--- tests/priestsMenu.spec.ts
+++ tests/priestsMenu.spec.ts
@@ -1,21 +1,23 @@
 import { test, expect } from "../fixtures/subpageMenu_2.spec";
-import { priestsMenuLinks } from "../data/priestsMenu.spec";
+import { JSONReader } from "../JSON-reader/JSONReader.spec";
 
 test.use({ mainMenuLink: 'Duszpasterze' });
 
+const testdata = JSONReader.get();
+
 test.describe('Priests menu links',async () => {
     
-    for(let i = 0; i < priestsMenuLinks.length; i++) {
+    for(let i = 0; i < testdata.priests.length; i++) {
 
-        test('Clicking the "' + priestsMenuLinks[i].link + '" link',async ({page, menu}) => {
+        test('Clicking the "' + testdata.priests[i].link + '" link',async ({page, menu}) => {
             
-            await test.step('Click the "' + priestsMenuLinks[i] + '" link',async () => {
+            await test.step('Click the "' + testdata.priests[i] + '" link',async () => {
                 
-                await menu.clickLink(priestsMenuLinks[i].link);
+                await menu.clickLink(testdata.priests[i].link);
             })
 
-            await expect(page).toHaveURL(priestsMenuLinks[i].pageUrl);
-            await expect(page).toHaveTitle(priestsMenuLinks[i].tabName);
+            await expect(page).toHaveURL(testdata.priests[i].pageUrl);
+            await expect(page).toHaveTitle(testdata.priests[i].tabName);
         })
     }
 })
