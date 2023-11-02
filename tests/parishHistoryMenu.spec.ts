838bc80b596082f4079aed381ff22cf8753e676c
aksi1984 <pablo_xmen@op.pl>
2023-11-02 16:55:03 +0100

    Test data from a json file was used

--- tests/parishHistoryMenu.spec.ts
+++ tests/parishHistoryMenu.spec.ts
@@ -1,21 +1,23 @@
-import { parishHistoryMenuLinks } from "../data/parishHistoryMenu.spec";
+import { JSONReader } from "../JSON-reader/JSONReader.spec";
 import { test, expect } from "../fixtures/subpageMenu_1.spec";
 
 test.use({ mainMenuLink: 'Historia parafii' });
 
+const testdata = JSONReader.get();
+
 test.describe('Parish history menu tests',async () => {
     
-    for(let i = 0; i < parishHistoryMenuLinks.length; i++) {
+    for(let i = 0; i < testdata.parishHistory.length; i++) {
 
-        test('Clicking the "' + parishHistoryMenuLinks[i].link + '" link',async ({page, menu}) => {
+        test('Clicking the "' + testdata.parishHistory[i].link + '" link',async ({page, menu}) => {
             
-            await test.step('Click the "' + parishHistoryMenuLinks[i].link + '" link',async () => {
+            await test.step('Click the "' + testdata.parishHistory[i].link + '" link',async () => {
                 
-                await menu.clickLink(parishHistoryMenuLinks[i].link);
+                await menu.clickLink(testdata.parishHistory[i].link);
             })
 
-            await expect(page).toHaveURL(parishHistoryMenuLinks[i].pageUrl);
-            await expect(page).toHaveTitle(parishHistoryMenuLinks[i].tabName);
+            await expect(page).toHaveURL(testdata.parishHistory[i].pageUrl);
+            await expect(page).toHaveTitle(testdata.parishHistory[i].tabName);
         })
     }
 })
