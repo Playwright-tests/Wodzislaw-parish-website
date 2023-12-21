95cf394a86f870c159724178ce70b3d45c9389bc
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-21 08:51:54 +0100

    Changed type name Link to Url

--- fixtures/subpageMenu_1.spec.ts
+++ fixtures/subpageMenu_1.spec.ts
@@ -2,9 +2,9 @@ import { test as base } from "@playwright/test";
 import { MainMenu } from "../page-object/elements/main-menu/mainMenu.spec";
 import { Menu_2 } from "../page-object/elements/menu_2.spec";
 
-export type Link = {
+export type Url = {
 
-    mainMenuLink: string;
+    url: string;
 };
 
 export type SubpageMenuFixture_1 = {
@@ -12,18 +12,16 @@ export type SubpageMenuFixture_1 = {
     menu: Menu_2
 }
 
-export const test = base.extend<Link & SubpageMenuFixture_1>({
+export const test = base.extend<Url & SubpageMenuFixture_1>({
 
-    mainMenuLink: ['Default link', {option: true}],
+    url: ['Default url', {option: true}],
 
-    menu:async ({page, mainMenuLink}, use) => {
+    menu:async ({page, url}, use) => {
        
-        await page.goto('https://wnmp.pl/');
-
         const menu = new Menu_2(page);
         const mainMenu = new MainMenu(page);
         
-        await mainMenu.clickLink(mainMenuLink);
+        await mainMenu.goto(url);
         await use(menu);
     }
 })
