85e1f66a6f3c7b372f89bdf2078568b2b1c4a057
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-21 08:28:47 +0100

    Created the enum

---
+++ enums/URLs.spec.ts
@@ -0,0 +1,9 @@
+export enum URLs {
+
+    HOME_PAGE = 'https://wnmp.pl/',
+    SACRAMENTS = 'https://wnmp.pl/informacje-stale/sakramenty-zasady-udzielania-terminy/',
+    PRIESTS = 'https://wnmp.pl/duszpasterze/obecni-duszpasterze/',
+    PARISH_GROUPS = 'https://wnmp.pl/grupy-parafialne/',
+    PARISH_HISTORY = 'https://wnmp.pl/historia-parafii/z-kart-historii/',
+    PHOTO_GALLERY = 'https://wnmp.pl/media/'
+}
