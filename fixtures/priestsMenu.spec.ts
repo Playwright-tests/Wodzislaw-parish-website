3caa60c363a7e6a4b28501c389b447d0327e9585
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-21 17:49:58 +0100

    Commented code removed

--- fixtures/priestsMenu.spec.ts
+++ fixtures/priestsMenu.spec.ts
@@ -1,19 +1,19 @@
+import { URLs } from "../enums/URLs.spec";
 import { test as base } from "../fixtures/mainMenu.spec";
-import { Menu_1 } from "../page-object/elements/menu_1.spec";
+import { Menu } from "../page-object/elements/menu.spec";
 
 type PriestsMenuFixture = {
 
-    priestsMenu: Menu_1
+    priestsMenu: Menu
 }
 
 export const test = base.extend<PriestsMenuFixture>({
 
     priestsMenu:async ({page, mainMenu}, use) => {
         
-        await page.goto('https://wnmp.pl/');
+        const priestsMenu = new Menu(page);
 
-        const priestsMenu = new Menu_1(page);
-
+        await mainMenu.goto(URLs.HOME_PAGE);
         await mainMenu.clickLink('Duszpasterze');
         await use(priestsMenu);
     }
