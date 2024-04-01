bb59a444a7c8049c2cbc506f3e32fc7a87892a58
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-04-01 13:12:16 +0200

    Created

---
+++ types/types.ts
@@ -0,0 +1,12 @@
+export type Link = {
+
+    link: string,
+    pageUrl: string,
+    tabName: string
+}
+
+export type PhotoGalleryData = {
+
+    partialLinkText: string,
+    partialUrl: string
+}
