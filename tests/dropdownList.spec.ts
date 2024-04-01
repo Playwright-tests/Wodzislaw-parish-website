8fffcbe772d97ee0281afad36c1650775ca8e3e2
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-04-01 12:48:32 +0200

    Created the dropdown list tests

---
+++ tests/dropdownList.spec.ts
@@ -0,0 +1,26 @@
+import { linkClickingAssertions } from "../common/assertions";
+import { test } from "../fixtures/mainMenu";
+import { getLinkTypes } from "../loaders/loaders";
+
+const links = getLinkTypes('dropdownList');
+
+test.describe('Main menu dropdown list',async () => {
+    
+    for(const link of links) {
+
+        test('Clicking the "' + link.link + '" link of the drop-down list',async ({mainMenu, page}) => {
+            
+            await test.step('Hover over the "Informacje stałe"',async () => {
+                
+                await (await mainMenu.getDropdownList()).hoverParent();
+            })
+
+            await test.step('Click the "' + link.link + '" link',async () => {
+                
+                await (await mainMenu.getDropdownList()).clickLink(link.link);
+            })
+
+            await linkClickingAssertions(page, link.pageUrl, link.tabName);
+        })
+    }
+})
