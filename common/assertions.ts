5a768609917703ea1d68ac1976f44c4277fdefbc
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-04-02 18:20:17 +0200

    Renamed the function

--- common/assertions.ts
+++ common/assertions.ts
@@ -1,6 +1,6 @@
 import { Page, expect } from "@playwright/test";
 
-export async function linkClickingAssertions(page: Page, expectedUrl: string, expectedTabName: string) {
+export async function redirectionLinkAssertion(page: Page, expectedUrl: string, expectedTabName: string) {
     
     await expect(page).toHaveURL(expectedUrl);
     await expect(page).toHaveTitle(expectedTabName);
