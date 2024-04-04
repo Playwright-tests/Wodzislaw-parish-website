eff80ba9fec0b3f11d409159e79cd2b84a7080b6
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-04-04 11:49:54 +0200

    Added the fixture

--- fixtures/mainMenu.ts
+++ fixtures/mainMenu.ts
@@ -1,10 +1,14 @@
 import { test as base } from "@playwright/test";
 import { MainMenu } from "../page-object/elements/main-menu/mainMenu.";
 import { URLs } from "../enums/URLs";
+import { MainMenuMobileMobileVersion } from "../page-object/elements/main-menu-mobile/mainMenuMobileMobileVersion";
 
 export { expect } from "@playwright/test";
 
-export const test = base.extend<{mainMenu: MainMenu}>({
+export const test = base.extend<{mainMenu: MainMenu} & {mainMenuMobileVersion: MainMenuMobileMobileVersion} &
+                                {isExpanded: boolean}>({
+
+    isExpanded: [false, {option: true}],
 
     mainMenu:async ({page}, use) => {
 
@@ -12,5 +16,17 @@ export const test = base.extend<{mainMenu: MainMenu}>({
         
         await mainMenu.goto(URLs.HOME_PAGE);
         await use(mainMenu);
+    },
+
+    mainMenuMobileVersion:async ({page, isExpanded}, use) => {
+        
+        const mainMenuMobileVersion = new MainMenuMobileMobileVersion(page);
+        await mainMenuMobileVersion.goto(URLs.HOME_PAGE);
+
+        if(isExpanded) {
+            await mainMenuMobileVersion.touchTriggerElement();
+        };
+
+        await use(mainMenuMobileVersion);
     }
 })
