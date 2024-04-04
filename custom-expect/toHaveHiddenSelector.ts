751caf4b5ca9174e2bce031caad525d5e712cfc9
pawelaksman <pablo_xmen@op.pl>
2024-04-04 14:56:25 +0200

    Created

---
+++ custom-expect/toHaveHiddenSelector.ts
@@ -0,0 +1,26 @@
+import { expect as baseExpect, Page } from "@playwright/test";
+
+export const expect = baseExpect.extend({
+
+    async toHaveHiddenSelector(page: Page, selector: string) {
+
+        const assertionName = 'toHaveHiddenSelector';
+        let pass: boolean;
+
+        try {
+            await page.waitForSelector(selector, {timeout: 6000, state: 'visible'});
+            pass = true;
+        } catch(e: any) {
+            pass = false
+        }
+
+        const message = pass ? () => 'The selector "' + selector + '" is hidden' :
+                               () => 'The selector "' + selector + '" is visible';
+
+        return {
+            name: assertionName,
+            pass,
+            message
+        }
+    }
+})
