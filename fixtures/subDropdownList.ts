9d37e3e1cd11ff633b4ba31fd5c34ccdc6e6b7a9
pawelaksman <pablo_xmen@op.pl>
2024-04-04 14:56:09 +0200

    Added the fixture

--- fixtures/subDropdownList.ts
+++ fixtures/subDropdownList.ts
@@ -1,17 +1,27 @@
 import { test as base } from "@playwright/test";
-import { SubDropdownList } from "../page-object/elements/main-menu-mobile/subDropDownList";
-import { MainMenuMobileMobileVersion } from "../page-object/elements/main-menu-mobile/mainMenuMobileMobileVersion";
+import { Submenu } from "../page-object/elements/main-menu-mobile-version/submenu";
+import { MainMenuMobileVersion } from "../page-object/elements/main-menu-mobile-version/mainMenuMobileMobileVersion";
 import { URLs } from "../enums/URLs";
 
 export { expect } from "@playwright/test";
 
-export const test = base.extend<{subDropdownList: SubDropdownList}>({
+export const test = base.extend<{subDropdownList: Submenu} & {expanded: Submenu}>({
 
     subDropdownList:async ({page}, use) => {
         
-        const mainMenuMobileVersion = new MainMenuMobileMobileVersion(page);
+        const mainMenuMobileVersion = new MainMenuMobileVersion(page);
         await mainMenuMobileVersion.goto(URLs.HOME_PAGE);
         await mainMenuMobileVersion.touchTriggerElement();
-        await use(new SubDropdownList(page));
+        await use(new Submenu(page));
+    },
+
+    expanded:async ({page}, use) => {
+        
+        const mainMenuMobileMobileVersion = new MainMenuMobileVersion(page);
+        const expanded = new Submenu(page);
+        await mainMenuMobileMobileVersion.goto(URLs.HOME_PAGE);
+        await mainMenuMobileMobileVersion.touchTriggerElement();
+        await expanded.touchArrow();
+        await use(expanded);
     }
 })
